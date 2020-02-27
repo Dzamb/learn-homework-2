a7cf7752597378467a44ed96de2b5e971f56df4e
Kozaev Dzambolat <otrina1@yandex.ru>
2020-02-27 23:50:46 +0300

    сделал задание по дате и времени

--- date_and_time.py
+++ date_and_time.py
@@ -8,21 +8,26 @@
 * Превратите строку "01/01/17 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
+import locale
+
+locale.setlocale(locale.LC_ALL, "russian")
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    date_today = datetime.now()
+    date_yesterday = date_today - timedelta(1)
+    date_month_ago = date_today - timedelta(30)
+    print(f"Сегодня: {date_today.strftime('%A %d %B %Y')}")
+    print(f"Вчера: {date_yesterday.strftime('%A %d %B %Y')}")
+    print(f"Месяц назад: {date_month_ago.strftime('%A %d %B %Y')}")
+
     
     
 def str_2_datetime(string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    date_string = "01/01/17 12:10:03.234567"
+    date_dt = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    return date_dt
+
 
 if __name__ == "__main__":
     print_days()
